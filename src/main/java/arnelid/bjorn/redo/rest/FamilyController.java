bcd849efeda416a2a53d4907272e0d5d42c7f4c2
Björn Arnelid <bjorn.arnelid@xylem.com>
2022-07-10 17:21:51 +0200

    Add delete mapping for family.

--- src/main/java/arnelid/bjorn/redo/rest/FamilyController.java
+++ src/main/java/arnelid/bjorn/redo/rest/FamilyController.java
@@ -2,6 +2,7 @@ package arnelid.bjorn.redo.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,15 +17,20 @@ import arnelid.bjorn.redo.repository.FamilyRepository;
 public class FamilyController {
 	@Autowired
 	FamilyRepository familyRepository;
+		
+	@PostMapping(path = "/family", consumes = MediaType.APPLICATION_JSON_VALUE)
+	void postFamily(@RequestBody Family newFamily) {
+		familyRepository.save(newFamily);
+	}
 	
-	
+	@DeleteMapping("/{name}")
+	void deleteFAmily(@PathVariable String name) {
+		Family toBeDeleted = familyRepository.findByName(name);
+		familyRepository.delete(toBeDeleted);
+	}
+
 	@GetMapping("/{name}")
 	Family getFamily(@PathVariable String name) {
 		return familyRepository.findByName(name);
-	}
-	
-	@PostMapping(path = "/Family", consumes = MediaType.APPLICATION_JSON_VALUE)
-	void postFamily(@RequestBody Family newFamily) {
-		familyRepository.save(newFamily);
 	}
 }
