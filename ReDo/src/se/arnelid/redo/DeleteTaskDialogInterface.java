546f15a54cbd4403e1ce3ef1a036c6e2d10f428c
Björn Arnelid <bjorn.arnelid@xylem.com>
2022-07-10 10:50:52 +0200

    Remove android sources.

--- ReDo/src/se/arnelid/redo/DeleteTaskDialogInterface.java
+++
@@ -1,9 +0,0 @@
-package se.arnelid.redo;
-
-import android.app.DialogFragment;
-
-public interface DeleteTaskDialogInterface {
-    public void onDialogPositiveClick(DialogFragment dialog, long id);
-    public void onDialogNegativeClick(DialogFragment dialog);
-
-}
